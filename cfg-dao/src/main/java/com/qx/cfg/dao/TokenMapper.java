218b4bb5c880aa9678248dbfff0fa3549755ba63
w77996 <912513404@qq.com>
2018-04-13 18:02:22 +0800

    token编写

---
+++ cfg-dao/src/main/java/com/qx/cfg/dao/TokenMapper.java
@@ -0,0 +1,17 @@
+package com.qx.cfg.dao;
+
+import com.qx.cfg.pojo.Token;
+
+public interface TokenMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Token record);
+
+    int insertSelective(Token record);
+
+    Token selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Token record);
+
+    int updateByPrimaryKey(Token record);
+}
